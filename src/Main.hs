bf21e217b36522b8248936385cb3385ada8e839c
Diego R. Ammirabile <diegoroberto.ammirabile@gmail.com>
2022-05-28 18:08:55 +0200

    Clock monitor funzionante

--- src/Main.hs
+++ src/Main.hs
@@ -7,17 +7,18 @@ import System.TellMe.Monitor.ACPI
 import System.TellMe.Hw
 import System.TellMe.Monitor.Workspaces
 import System.TellMe.Monitor.PulseAudio
+import System.TellMe.Monitor.Clock
 
 -- import System.TellMe.Monitor.Clock
 
 main :: IO ()
 main = do
---  w1 <- clockWidget "%c"
+  w1 <- clockWidget "%c"
   w2 <- batteryWidget
   hw <- helloWord
   v <- pulseAdioWidget 
   --ws <- workspaces
   tellme $ def { startWidgets = [v]
-               , endWidgets = [w2]
+               , endWidgets = [w2,w1]
                , barPosition = Top
                }
--- src/System/TellMe/Monitor/Clock.hs
+++ src/System/TellMe/Monitor/Clock.hs
@@ -2,9 +2,9 @@ module System.TellMe.Monitor.Clock where
 
 import Graphics.UI.Gtk (Widget)
 import Data.Time.Clock (getCurrentTime)
-import Data.Time.Format (formatTime)
+import Data.Time.Format (formatTime,defaultTimeLocale)
 import Data.Time.LocalTime (LocalTime, getCurrentTimeZone, utcToLocalTime)
-import System.Locale (defaultTimeLocale)
+-- import System.Locale (defaultTimeLocale)
 import System.TellMe.Monitor
 
 tick :: IO LocalTime
@@ -13,9 +13,7 @@ tick = do
   now <- getCurrentTime
   return $ utcToLocalTime tz now
 
-  {--
 clockWidget :: String -> IO Widget
 clockWidget format = periodic_ 1000 tick --> m
   where
     m = formatTime defaultTimeLocale format >$< mkText
-    --}
